ef7b0b74c25ae54d1991720fd6a9b0797a0de8f4
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-09-30 21:54:08 +0530

    Update Factorial.java
    Co-authored-by: test-pr-genie[bot] <232575029+test-pr-genie[bot]@users.noreply.github.com>

--- Factorial.java
+++ Factorial.java
@@ -9,7 +9,7 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
-        int n = sc.nextInt();
+        int inputNumber = sc.nextInt();
         System.out.println("Factorial: " + fact(n));
     }
 }
