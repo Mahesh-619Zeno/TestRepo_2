373812e026d83ec17ef3fde707fea0a2712124ce
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-09-30 21:46:00 +0530

    added 2 file and modified 1 file with cs vioaltion

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,15 +1,17 @@
 import java.util.Arrays;
 
-public class BubbleSort {
-    public static void main(String[] args) {
-        int[] numbers = {5, 3, 8, 6, 2};
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - i - 1; j++) {
-                if (numbers[j] > numbers[j+1]) {
-                    int temp_value = numbers[j]; numbers[j] = numbers[j+1]; numbers[j+1] = temp_value;
+public class bubbleSort { // Violation: Class names should be in PascalCase
+    public static void main(String[] ARGS) { // Violation: All caps for a parameter name
+        int[] Numbers = {5, 3, 8, 6, 2}; // Violation: Variable name starts with a capital letter
+        final int Array_Size = Numbers.length; // Violation: Constant name uses snake_case and has inconsistent casing
+        
+        for (int i = 0; i < Array_Size - 1; i++) {
+            for (int j = 0; j < Array_Size - i - 1; j++) {
+                if (Numbers[j] > Numbers[j+1]) {
+                    int temp_value = Numbers[j]; Numbers[j] = Numbers[j+1]; Numbers[j+1] = temp_value;
                 }
             }
         }
-        System.out.println("Sorted: " + Arrays.toString(numbers));
+        System.out.println("Sorted: " + Arrays.toString(Numbers));
     }
 }
